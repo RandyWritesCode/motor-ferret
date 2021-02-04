0ead8b3a0a8fbe284122cb9165f513f8e510026b
Randy Douglas <randy.e.douglas@gmail.com>
2021-02-04 09:23:17 -0500

    update endpoint in config.

--- src/config.js
+++ src/config.js
@@ -1,13 +1,9 @@
 const config = {
   API_ENDPOINT:
-    //   'http://localhost:8000/api'
-    // || 
-    'https://mighty-forest-27334.herokuapp.com/api'
-  ,
+    'https://mighty-forest-27334.herokuapp.com/api',
   TOKEN_KEY:
     'motor-ferret-client-auth-token',
   USER_ID_KEY: 'USER_ID_KEY',
   ADMIN_AUTH_KEY: 'ADMIN_AUTH_KEY'
 }
 
-export default config
