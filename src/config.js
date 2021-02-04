8228176f8fe77e18d8a8fc9e6ab2b17d50d46765
Randy Douglas <randy.e.douglas@gmail.com>
2021-02-04 08:50:28 -0500

    altered endpoint

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
   API_ENDPOINT:
-    'http://localhost:8000/api'
-  // || 
-  // 'https://mighty-forest-27334.herokuapp.com/'
+    //   'http://localhost:8000/api'
+    // || 
+    'https://mighty-forest-27334.herokuapp.com/api'
   ,
   TOKEN_KEY:
     'motor-ferret-client-auth-token',
